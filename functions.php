d3dbcb0bec4d11ce60f713d07c72d1742c99ed5e
DEKE ZHANG <s3755861@student.rmit.edu.au>
2021-05-16 11:54:28 +0800

    update

--- functions.php
+++ functions.php
@@ -122,11 +122,10 @@ try {
 }
 }
 
- function createTrip($driver, $passenger, $startlat, $starlng, $deslat, $deslat){
+ function createTrip($driver, $passenger, $status, $startlat, $startlng, $deslat, $deslng){
     $url = "https://q7sqmxgvv6.execute-api.us-east-1.amazonaws.com/trip";
-    $tripID = new time();
     $params = json_encode(
-    array('tripID' => tripID, 'driver' => $driver, 'passenger' => $passenger, 'status' => 'pending', 'startlat' => $startlat, 
+    array('tripID' => time(), 'driver' => $driver, 'passenger' => $passenger, 'status' => $status, 'startlat' => $startlat, 
     'startlng' => $startlng, 'deslat' => $deslat, 'deslng' => $deslng)
     );
      
@@ -143,6 +142,89 @@ try {
      
     $res = curl_exec($ch);
     curl_close($ch);
+ }
+
+function editTrip($tripID, $driver, $passenger, $status, $startlat, $startlng, $deslat, $deslng){
+    $url = "https://q7sqmxgvv6.execute-api.us-east-1.amazonaws.com/trip";
+    $tripID = new time();
+    $params = json_encode(
+    array('tripID' => $tripID, 'driver' => $driver, 'passenger' => $passenger, 'status' => $status, 'startlat' => $startlat, 
+    'startlng' => $startlng, 'deslat' => $deslat, 'deslng' => $deslng)
+    );
+     
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($params)
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+     
+    $res = curl_exec($ch);
+    curl_close($ch);
+ }
+ 
+
+ function deleteTrip($tripID, $driver){
+    $url = "https://q7sqmxgvv6.execute-api.us-east-1.amazonaws.com/trip";
+    $params = json_encode(
+    array('tripID' => $tripID, 'driver' => $driver)
+    );
+     
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($params)
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+     
+    $res = curl_exec($ch);
+    curl_close($ch);
+ }
+
+ function pendingTrip($dynamodb, $marshaler, $driver){
+    $eav = $marshaler->marshalJson('
+    {
+        ":driver":"'.$driver.'",
+        ":sta":"pending"
+    }
+    ');
+
+    $params = [
+    'TableName' => 'Trip',
+    'FilterExpression' => '#sta=:sta and driver=:driver',
+    'ExpressionAttributeNames'=> [ '#sta' => 'status' ],
+    'ExpressionAttributeValues'=> $eav
+    ];
+
+try {
+    $result = $dynamodb->scan($params);
+    if(empty($result['Items'])){
+       return false;
+    }else{
+        foreach ($result['Items'] as $trip) {
+        $array[0] = $marshaler->unmarshalValue($trip['tripID']);
+        $array[1] = $marshaler->unmarshalValue($trip['driver']);
+        $array[2] = $marshaler->unmarshalValue($trip['passenger']); 
+        $array[3] = $marshaler->unmarshalValue($trip['status']); 
+        $array[4] = $marshaler->unmarshalValue($trip['startlat']); 
+        $array[5] = $marshaler->unmarshalValue($trip['startlng']);  
+        $array[6] = $marshaler->unmarshalValue($trip['deslat']); 
+        $array[7] = $marshaler->unmarshalValue($trip['deslng']);    
+    }
+        return $array;
+    }
+} catch (DynamoDbException $e) {
+    echo "Unable to query:\n";
+    echo $e->getMessage() . "\n";
+}
  }
 
 ?>
