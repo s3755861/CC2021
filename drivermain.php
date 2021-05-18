f09853a0ddd9e1661026ddad87580ab1bb620976
DEKE ZHANG <s3755861@student.rmit.edu.au>
2021-05-18 13:11:16 +0800

    update

--- drivermain.php
+++ drivermain.php
@@ -13,29 +13,47 @@
     <?php
       require "functions.php";
       session_start();
+      $boo;
+      $arr;
       $username = $_SESSION['user'];
       $user = getLocation($dynamodb, $marshaler, $username);
       $lat = $marshaler->unmarshalValue($user['lat']);
       $lng = $marshaler->unmarshalValue($user['lng']);  
-      $trip = pendingTrip($dynamodb, $marshaler, $username);
+      $trip = pendingTrip($dynamodb, $marshaler, 'vcdgfdh');
+      if(empty($trip)){
+        $boo = -1;
+      }else{
+        $boo = 1;
+        $arr = json_encode($trip);
+      }
+
     ?>
       
-    <script>
-    var boo = <?php echo validate($trip);?>;
-    if(boo == 1){
-      accept=window.confirm("A passenger wants to have a trip, do you want to help them?");
-      if(accept == true){
-          <?php 
-          $trip[3] = 'inprogress';
-          editTrip($trip[0], $trip[1], $trip[2], $trip[3], $trip[4], $trip[5], $trip[6], $trip[7]);
-          ?>
-      }else{
-          <?php 
-          deleteTrip($trip[0], $trip[1]);
-          ?>
-          }
-      }
-    </script>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
+    var abc = <?php echo $boo;?>;
+    if(abc == 1){
+      var a = confirm("A passenger wants to have a trip, do you want to help them?");
+      var arr = <?php echo $arr;?>;
+      arr[3] = "inprogress";
+    if(a){
+      $.ajax({
+        url: 'editTrip.php',
+        type: 'post',
+        data: {tripID: arr[0],driver:arr[1],passenger:arr[2],status:arr[3],startlat:arr[4],startlng: arr[5],deslat:arr[6],deslng: arr[7]},
+        dataType: 'JSON'
+      });
+    }else{
+        $.ajax({
+        url: 'deleteTrip.php',
+        type: 'post',
+        data: {tripID:arr[0],driver:arr[1]},
+        dataType: 'JSON'
+        })
+    }
+    }
+
+</script>
 
     <script>
       
